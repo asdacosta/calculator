6578eb5c4c43dbe36fb001278631cca55fb95946
asdacosta <asdacosta18@gmail.com>
2023-08-21 02:46:08 -0700

    Make first and second numberss entered be stored in their respective variables

--- calc.js
+++ calc.js
@@ -14,10 +14,6 @@ function div(num1, num2) {
     return num2 === 0 ? "Impossible!" : num1 / num2;
 }
 
-let firstNum = 0,
-    operator = '',
-    secNum = 0;
-
 function operate(num1, sign, num2) {
     if (sign === '+') {
         return add(num1, num2);
@@ -33,33 +29,61 @@ function operate(num1, sign, num2) {
 let  currentValue = 0;
 let box = document.querySelector('.result');
 
-let firstClickNum = true;
 let numButtons = document.querySelectorAll('.light button, .light2 button');
-firstNum = numButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        if (firstClickNum) {
-            box.innerHTML = '';
-            firstClickNum = false;
-        }
-        if (button.textContent === '.') {
-            button.disabled = true;
-        }
+let firstClickNum = true;
 
-        currentValue = button.textContent;
-        box.textContent += currentValue;
+function setupNumButtonClick () {
+    numButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            if (firstClickNum) {
+                box.innerHTML = '';
+                firstClickNum = false;
+            }
+            if (button.textContent === '.') {
+                button.disabled = true;
+            }
+
+            if (firstClickSign) {
+                if (secNum === null) {
+                    box.innerHTML = '';
+                }
+                secNum = box.textContent + button.textContent;
+            }
+                
+            currentValue = button.textContent;
+            box.textContent += currentValue;
+        })
     })
-})
+}
+
+let firstNum = 0,
+    operator = '',
+    secNum = null;
+
+setupNumButtonClick();
+firstClickSign = false;
 
 let signButtons = document.querySelectorAll('.right button');
-operator = signButtons.forEach(sign => {
+signButtons.forEach(sign => {
     sign.addEventListener('click', () => {
         signButtons.forEach(signs => {
         signs.style.backgroundColor = '';
         signs.style.color = '';
         signs.style.fontWeight = '';
         })
+
         sign.style.backgroundColor = 'white';
         sign.style.color = 'orange';
         sign.style.fontWeight = 'bold';
+
+        if (!firstClickSign) {
+            firstNum = box.textContent;
+            firstClickSign = true;
+        } else {
+            secNum = box.textContent;
+            box.innerHTML = '';
+        }
+
+        operator = sign.textContent;
     })
 })
